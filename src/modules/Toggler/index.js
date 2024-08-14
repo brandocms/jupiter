47de11f1f07f554017740557644336cc830e9c70
T. Mjoen <trond@twined.net>
2024-08-14 14:47:04 +0200

    Toggler: Verify we have triggerIcon

--- src/modules/Toggler/index.js
+++ src/modules/Toggler/index.js
@@ -1,14 +1,11 @@
 import { gsap } from 'gsap'
 import Dom from '../Dom'
 
-const DEFAULT_OPTIONS = {}
-
 export default class Toggler {
   constructor(app, el) {
     this.open = false
     this.app = app
     this.el = el
-    // this.opts = _defaultsDeep(opts, DEFAULT_OPTIONS)
     this.trigger = Dom.find(this.el, '[data-toggle-trigger]')
     this.triggerIcon = Dom.find(this.trigger, 'span.icon')
     this.content = Dom.find(this.el, '[data-toggle-content]')
@@ -19,12 +16,16 @@ export default class Toggler {
     this.toggleState()
 
     if (this.open) {
-      this.triggerIcon.classList.toggle('active')
-      gsap.set(this.content, { height: 'auto' })
+      if (this.triggerIcon) {
+        this.triggerIcon.classList.toggle('active')
+      }
+      gsap.set(this.content, { height: 'auto', display: 'block' })
       this.el.classList.toggle('open')
       gsap.from(this.content, { height: 0, ease: 'power1.inOut' })
     } else {
-      this.triggerIcon.classList.toggle('active')
+      if (this.triggerIcon) {
+        this.triggerIcon.classList.toggle('active')
+      }
       gsap.to(this.content, {
         duration: 0.25,
         onComplete: () => {
